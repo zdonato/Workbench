02234cd01803a68bcaaaa6e1c6d226368cf7c1b8
Zachary Donato <zdonato@stevens.edu>
2014-01-09 13:27:07 -0500

    Added getName, chooseMove, and various fixes

--- GamePlay/Player.java
+++ GamePlay/Player.java
@@ -1,30 +1,60 @@
 // Player class. 
 import java.util.Random; 
 import java.util.TreeMap; 
+import java.util.Scanner; 
 
 public class Player{
 	// Initialize variables. 
-	private double maxDmg; 
+	private int maxDmg; 
 	private String name; 
-	private double health; 
+	private static int health; 
 	private static TreeMap<String, String> food = new TreeMap<String, String>();
+	private int dmg; 
+	private Random generator = new Random();  
 	
 	// Constructor. 
-	public Player(double md, String n, double h){
+	public Player(int md, String n, int h){
 		this.maxDmg = md;  
 		this.name = n; 
 		this.health = h; 
 	} // End Constructor. 
 	
+	// Method chooseMove for textual interface. 
+	public void chooseMove(){
+		Scanner scan = new Scanner(System.in); 
+		System.out.println("Choose a move by typing an option from the list below: (Type 0 to quit)"); 
+		System.out.println("Attack | Heal | Pass");
+		String choice = scan.next(); 
+		if ("Attack".equals(choice) || "attack".equals(choice)){
+			dmg = generator.nextInt(maxDmg);
+			dmgDealt(dmg); 
+			System.out.println("Your current health is: " + getHealth()); 
+		} else if ("Heal".equals(choice) || "heal".equals(choice)){
+			System.out.println("Choose what to heal with: (Type 0 to quit)"); 
+			System.out.println("Lobster | Shark | Shrimp"); 
+			String foodChoice = scan.next(); 
+			if ("0".equals(foodChoice)){
+				health = 0; 
+			} else {
+				heal(foodChoice); 
+				System.out.println(getHealth()); 
+			}
+		} else if ("0".equals(choice)){
+			health = 0; 
+		} else {
+			System.out.println("Invalid Command. Choose again."); 
+		}
+	} // End chooseMove. 
+	
 	// Method dmgDealt for receiving damage. 
-	public void dmgDealt(double dmg){
-		health -= dmg; 
+	public void dmgDealt(int damage){
+		health -= damage; 
 	} // End dmgDealth. 
 	
 	// Method heal for healing the player. 
 	public void heal(String foodName){
 		String f = food.get(foodName); 
-		double n = Double.parseDouble(f); 
+		double n = Integer.parseInt(f); 
 		health += n; 
 	} // End heal. 
 	
@@ -33,14 +63,26 @@ public class Player{
 		return health;  
 	} // End getHealth. 
 	
+	public String getName(){
+		return name; 
+	}
+	
 	// Main method. 
 	public static void main(String[] args){
 		food.put("Lobster", "240"); 
 		food.put("Shark", "500"); 
-		Player Zach = new Player(200, "Zach", 1000); 
-		Zach.dmgDealt(400); 
-		System.out.println(Zach.getHealth()); 
-		Zach.heal("Lobster"); 
-		System.out.println(Zach.getHealth()); 
+		food.put("Shrimp", "100"); 
+		Player Zach = new Player(100, "Zach", 1000); 
+		Player Liz = new Player(100, "Liz", 1000); 
+		Player currPlayer = Zach; 
+		while (health > 0){
+			currPlayer.chooseMove(); 
+			if (currPlayer.getName().equals(Zach)){
+				currPlayer = Liz; 
+			} else {
+				currPlayer = Zach; 
+			}
+		}
+		
 	}
 }

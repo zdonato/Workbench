71550a899d563c874f9ba7d93350a1f250c7be0a
Zachary Donato <zdonato@stevens.edu>
2014-01-09 21:15:45 -0500

    Finishing touches to player class and playGame

--- GamePlay/Player.java
+++ GamePlay/Player.java
@@ -1,14 +1,23 @@
-// Player class. 
-import java.util.Random; 
-import java.util.TreeMap; 
+// Developer: Zachary Donato 
+// Date: 1.8.13 
+import java.util.Random;  
 import java.util.Scanner; 
 
+// A class to create a player. 
 public class Player{
 	// Initialize variables. 
 	private int maxDmg; 
 	private String name; 
 	private int health; 
-	private static TreeMap<String, String> food = new TreeMap<String, String>();
+	// The available foods. Attempted to use TreeMap, but method heal malfcuntioned. 
+	// Reminder to try that again at a later time. 
+	private int lobster = 200; 
+	private int shark = 500; 
+	private int shrimp = 100; 
+	// Variables to keep count of the available supplies during a session. 
+	private int numL = 5; 
+	private int numShark = 2; 
+	private int numShrimp = 10; 
 	
 	// Constructor. 
 	public Player(int md, String n, int h){
@@ -24,14 +33,42 @@ public class Player{
 	
 	// Method heal for healing the player. 
 	public void heal(String foodName){
-		String f = food.get(foodName); 
-		int n = Integer.parseInt(f); 
-		health = health + n; 
+		if ("Lobster".equals(foodName) || "lobster".equals(foodName)){
+			if (numL > 0){
+				health += lobster; 
+				numL--;
+				System.out.println("You have " + numL + " lobsters left."); 
+			 } else {
+				System.out.println("Sorry, you are out of lobsters."); 
+			}
+		} else if ("Shark".equals(foodName) || "shark".equals(foodName)){
+			if (numShark > 0){
+				health += shark; 
+				numShark--; 
+				System.out.println("You have " + numShark + " sharks left."); 
+			} else { 
+				System.out.println("Sorry, you are out of sharks."); 
+			}
+		} else if ("Shrimp".equals(foodName) || "shrimp".equals(foodName)){
+			if (numShrimp > 0){
+				health += shrimp; 
+				numShrimp--; 
+				System.out.println("You have " + numShrimp + " shrimp left."); 
+			} else {
+				System.out.println("Sorry, you are out of shrimp."); 
+			}
+		} else { 
+			System.out.println("Sorry you do not have any " + foodName + ". You forfeit your turn."); 
+		}
 	} // End heal. 
 	
 	// Method getHealth to return the health of the player. 
-	public double getHealth(){
+	public int getHealth(){
+		if (health < 0){ 
+			return 0; 
+		} else {
 		return health;  
+		}
 	} // End getHealth. 
 	
 	// Method getName. 
@@ -43,10 +80,4 @@ public class Player{
 	public int getMaxDmg(){
 		return maxDmg; 
 	} // End getMaxDmg. 
-	
-	public static void main(String[] args){
-		food.put("Lobster", "240"); 
-		food.put("Shark", "500"); 
-		food.put("Shrimp", "100"); 
-	}
 }
--- GamePlay/playGame.java
+++ GamePlay/playGame.java
@@ -1,76 +1,78 @@
+// Developer: Zachary Donato
+// Date: 1.8.14
+
 import java.util.Random; 
-import java.util.TreeMap;
 import java.util.Scanner; 
 
+// A class to play the game. 
 public class playGame {
-	private static TreeMap<String, String> food = new TreeMap<String, String>();
-	static boolean loopCheck = true; 
-	static boolean playCheck = true;
+	static boolean turnCheck = true; // Bool value to ensure the player has made a move before switching to the other player's turn. 
 	private static int dmg; 
 	private static Random generator = new Random(); 
 	
 	public static void main(String[] args) {
-		// Add items to food to use for healing. 
-		food.put("Lobster", "240"); 
-		food.put("Shark", "500"); 
-		food.put("Shrimp", "200"); 
 		// Create two players. 
-		Player Zach = new Player(1000, "Zach", 1000); 
-		Player Liz = new Player(1000, "Liz", 1000);
+		Player player1 = new Player(100, "Zach", 1000); 
+		Player player2 = new Player(100, "Liz", 1000);
 		// Variables to keep tract of who deals and receives damage. 
 		Player currPlayer = Zach; 
 		Player other = Liz; 
 		// While loop to create the textual interface of the game. 
-		while (loopCheck){
+		while (true){
 			System.out.println(currPlayer.getName() + " it is your turn."); 
-			Scanner scan = new Scanner(System.in); 
+			Scanner scan = new Scanner(System.in); // Scanner to read input. 
 			System.out.println("Choose a move by typing an option from the list below: (Type 0 to quit)"); 
 			System.out.println("Attack | Heal | Pass");
-			String choice = scan.next(); 
+			String choice = scan.next(); // Read the user's choice as a string value on the next line. 
 			if ("Attack".equals(choice) || "attack".equals(choice)){
-				dmg = generator.nextInt(currPlayer.getMaxDmg());
+				// Generate a random number to be applied as damage. 
+				dmg = generator.nextInt(currPlayer.getMaxDmg()) + 1; 
 				System.out.println("You dealt " + dmg +" damage to " + other.getName() ); 
-				other.dmgDealt(dmg); 
+				other.dmgDealt(dmg); // Apply the damage. 
 				System.out.println("Your current health is: " + currPlayer.getHealth()); 
 				System.out.println(other.getName() + "'s current health is: " + other.getHealth()); 
-				playCheck = true; 
+				turnCheck = true; // Allow switch to other player's turn. 
 			} else if ("Heal".equals(choice) || "heal".equals(choice)){
 				System.out.println("Choose what to heal with: (Type 0 to quit)"); 
-				System.out.println("Lobster | Shark | Shrimp"); 
-				Scanner foodScan = new Scanner(System.in); 
-				String foodChoice = foodScan.next(); 
+				// List of options to choose to heal with. Expandable. 
+				System.out.println("Lobster (200) | Shark (500) | Shrimp (100) | Back (To go back)"); 
+				Scanner foodScan = new Scanner(System.in); // New Scanner to read the user's choice. 
+				String foodChoice = foodScan.next(); // Read the next line as a string value. 
 				if ("0".equals(foodChoice)){
-					loopCheck = false; 
-				} else if (!food.containsKey(foodChoice)) {
-					System.out.println("That food is not available. Choose again."); 
-					playCheck = false; 
+					break;  // End the loop and game if 0 is the input. 
+				} else if ("Back".equals(foodChoice) || "back".equals(foodChoice)){ // Option to return to main menu. 
+					System.out.println("Taking you back to the main menu... "); 
+					turnCheck = false; // Go back to the main menu without switching turns. 
 				} else {
-					currPlayer.heal(foodChoice); 
-					System.out.println(currPlayer.getHealth()); 
-					playCheck = true; 
+					currPlayer.heal(foodChoice); // Heal the player based on choice. The heal method will check for invalid inputs. 
+					System.out.println("Your current health is: " + currPlayer.getHealth()); 
+					System.out.println(other.getName() + "'s current health is: " + other.getHealth()); 
+					turnCheck = true; // Allow for turn switch. 
 				}
 			} else if ("0".equals(choice)){
-				loopCheck = false; 
-			} else if ("Pass".equals(choice) || "pass".equals(choice)){
-				System.out.println("You choose to pass."); 
-			} else {
-				System.out.println("Invalid Command. Choose again."); 
-				playCheck = false;
+				break; // End the loop if 0 is chosen.  
+			} else if ("Pass".equals(choice) || "pass".equals(choice)){ // Option to pass if no action is desired.
+				System.out.println("You choose to pass. Switching turns..."); 
+			} else { // Covers any other case that is not a valid input. Except for an enter command. 
+				System.out.println("Invalid Command. Choose again..."); 
+				turnCheck = false; // Go back to main menu without switching turns. 
 			}
-			if (currPlayer.getHealth() < 0){
+			// Cases to determine if there is a win. 
+			if (currPlayer.getHealth() <= 0){
 				System.out.println("Congratulations! " + other.getName() + " wins!");
 				break;
-			} else if (other.getHealth() < 0){
+			} else if (other.getHealth() <= 0){
 				System.out.println("Congratulations! " + currPlayer.getName() + " wins!"); 
 				break; 
 			}
-			if (currPlayer.equals(Zach) && playCheck){
+			// Cases to switch turns only if turnCheck = true. 
+			if (currPlayer.equals(Zach) && turnCheck){
 			currPlayer = Liz; 
 			other = Zach; 
-			} else if(currPlayer.equals(Liz) && playCheck){
+			} else if(currPlayer.equals(Liz) && turnCheck){
 				currPlayer = Zach; 
 				other = Liz; 
 			}
 		}
-	}
-}
+	} // End main. 
+} // End class playGame. 

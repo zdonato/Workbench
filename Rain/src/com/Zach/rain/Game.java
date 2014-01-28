0c15309c42542867eee7d9ae6af084fde471490b
Zachary Donato <zdonato@stevens.edu>
2014-01-28 12:46:21 -0500

    Added some methods

--- Rain/src/com/Zach/rain/Game.java
+++ Rain/src/com/Zach/rain/Game.java
@@ -1,7 +1,39 @@
 package com.Zach.rain;
 
-public class Game {
+import java.awt.Canvas;
+import javax.swing.JFrame;
+
+
+public class Game extends Canvas implements Runnable{
 	public static int width = 300; 
 	public static int height = (width/16) * 9; 
 	public static int scale = 3; 
+	
+	private Thread thread; 
+	private JFrame frame; 
+	private boolean running = false; 
+	
+	public Game(){
+		
+	}
+	public synchronized void start(){
+		running = true; 
+		thread = new Thread(this, "Display");
+		thread.start(); 
+	}
+	
+	public synchronized void stop(){
+		running = false;
+		try { 
+			thread.join();
+		} catch (InterruptedException e){
+			e.printStackTrace();
+		}
+	}
+	
+	public void run(){
+		while(running){
+			
+		}
+	}
 }

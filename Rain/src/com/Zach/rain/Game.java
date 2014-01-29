6bb9ebf3b05f45ee61561950e09ea55f3dbe2d4f
Zachary Donato <zdonato@stevens.edu>
2014-01-28 21:10:54 -0500

    Constructed the JFrame.

--- Rain/src/com/Zach/rain/Game.java
+++ Rain/src/com/Zach/rain/Game.java
@@ -1,10 +1,13 @@
 package com.Zach.rain;
 
 import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension; 
 import javax.swing.JFrame;
 
 
 public class Game extends Canvas implements Runnable{
+	private static final long serialVersionUID = 1L; 
 	public static int width = 300; 
 	public static int height = (width/16) * 9; 
 	public static int scale = 3; 
@@ -14,7 +17,10 @@ public class Game extends Canvas implements Runnable{
 	private boolean running = false; 
 	
 	public Game(){
+		Dimension size = new Dimension(width*scale, height*scale); 
+		setPreferredSize(size); 
 		
+		frame = new JFrame(); 
 	}
 	public synchronized void start(){
 		running = true; 
@@ -33,7 +39,20 @@ public class Game extends Canvas implements Runnable{
 	
 	public void run(){
 		while(running){
-			
+
 		}
+	}
+	
+	public static void main(String[] args){ 
+		Game game = new Game(); 
+		game.frame.setResizable(false);
+		game.frame.setTitle("Rain"); 
+		game.frame.add(game); 
+		game.frame.pack(); 
+		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+		game.frame.setLocationRelativeTo(null);
+		game.frame.setVisible(true); 
+		
+		game.start(); 
 	}
 }

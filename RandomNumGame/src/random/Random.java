6c4ea207f80efb42b1737567d83dab32d0af7e96
Zachary Donato <zdonato@stevens.edu>
2014-03-12 17:47:55 -0400

    Initial commit. Random Number Guessing Game.

---
+++ RandomNumGame/src/random/Random.java
@@ -0,0 +1,39 @@
+package random;
+
+// Imports.
+import javax.swing.*;
+import java.awt.Event.*;
+import java.awt.Color.*;
+import java.awt.*; 
+
+/**
+ * A game in which the user tries to guess a random number.
+ * @author Zachary Donato
+ * Date: 3.12.14
+ * Revisions: 
+ */
+
+public class Random extends JFrame{
+	// Initialize variables.
+	private static final long serialVersionUID = 1L;
+	private JLabel guessLabel, result, randomNum, title; 
+	private JTextField guess; 
+	private JButton submit; 
+	
+	// Constructor.
+	
+	public Random(){
+		// Set layout and declare variables.
+		setLayout(new GridBagLayout());
+		GridBagConstraints g = new GridBagConstraints(); 
+		getContentPane().setBackground(Color.gray);
+		
+		// Set up the JLabels, textfield, and button.
+		JLabel title = new JLabel("Welcome to the Random Number Game!"); 
+		
+		
+		
+	}
+	
+	
+}
